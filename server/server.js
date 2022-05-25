60c0ca94da5084924e84a65084ece495182b07db
mafihawe <mfhw@online.de>
2022-05-25 11:07:22 +0200

    port change

--- server/server.js
+++ server/server.js
@@ -6,6 +6,6 @@ const app = express();
 app.use(express.json());
 app.use('/api', apiRouter);
 app.use('/auth', authRouter);
-app.listen('4000', () => {
-    console.log('Server is running on port 4000');
+app.listen('5000', () => {
+    console.log('Server is running on port 5000');
 });
