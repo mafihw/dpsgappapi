870455a154fcdfaf57403f39ceda4f88508ba305
Marco <97028409+mafihw@users.noreply.github.com>
2022-06-01 21:23:10 +0200

    added installer

---
+++ installer.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+# This script installs the API
+# All questions during the installation (e.g. "Which services should be restarted?") can be answered with yes/okay without making changes
+# This script was tested with Ubuntu Server and Raspian on a RaspberryPi
+
+# UPDATE THE SYSTEM
+sudo apt update -y
+sudo apt upgrade -y
+
+# INSTALL DEPENDENCIES
+sudo apt-get install mariadb-server -y
+curl -sL https://deb.nodesource.com/setup_16.x -o nodesource_setup.sh
+sudo bash nodesource_setup.sh
+sudo rm nodesource_setup.sh
+sudo apt-get install nodejs -y
+sudo apt-get install git -y
+
+# DOWNLOAD API SOURCECODE
+git clone https://github.com/mafihw/dpsgappapi.git
+cd dpsgappapi
+
+# SETUP THE DATABASE
+sudo mysql < dbconfig.txt
+
+# INSTALL NODE PACKAGES
+sudo npm install -g nodemon -y
+sudo npm install -y
+sudo npm install -g pm2 -y
+
+# START AND DEAMONIZE THE SERVER
+sudo pm2 startup | bash
+sudo pm2 start server/server.js
+sudo pm2 save
