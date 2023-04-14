d6ecca0e58384fa8fd334b040a1ea12e024dc171
Jannis <jannismueller2001@gmail.com>
2023-04-14 14:35:00 +0200

    changes in config

--- server/config.js
+++ server/config.js
@@ -1,7 +1,7 @@
 var config = {};
 
 config.port = 3000;
-config.tokenExpiration = '1h';
+config.tokenExpiration = '30d';
 config.refreshTokenExpiration = '180d';
 config.secretRefreshKey = 'SECRETREFRESHKEY';
 config.secretKey = 'SECRETKEY';
